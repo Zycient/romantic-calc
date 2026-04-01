8f6cb1bd67955a2298d5f98125fd8cd47eb5b2f5
Maxwell Crawford <crawfordmaxwell24@gmail.com>
2026-04-01 11:43:36 -0400

    chore: init vite proj with sane defaults

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg' // ? TODO: replace with free calc logo
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          {/* // ? TODO: update img with calc logo and re-enable */}
+          {/* <img src={reactLogo} className="framework" alt="React logo" /> */}
+        </div>
+        <div>
+          <h1>Romantic Calc</h1>
+          <p>
+            Calc TODO...
+          </p>
+        </div>
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
